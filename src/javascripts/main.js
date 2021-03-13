2f1e286fdef2975807a1ec6022d49e36fdeab1fd
TakuyaOshima <takuyaoshima@TakuyaOshimanoMacBook-Pro.local>
2021-03-13 15:21:55 +0900

    Clean up project

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,18 +1,3 @@
 import './reactApp.jsx';
-import my from './my.js';
 import '../stylesheets/main.scss';
 
-import Vue from 'vue';
-import VueApp from './VueApp.vue';
-
-import add from './add.ts';
-
-new Vue({
-  el: '#vue-root',
-  render: (h) => h(VueApp),
-});
-
-console.log(add(a, 9));
-
-console.log('This is index.js');
-my();
